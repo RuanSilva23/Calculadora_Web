e10de060b0ee678a33fdbc2c11581303e43f2bf0
Ruan Silva <ruan.silva2002@gmail.com>
2025-03-03 14:06:32 -0300

    Escrevi o script

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+
+function addToDisplay(value) {
+    display.value += value;
+}
+
+
+function clearDisplay() {
+    display.value= ""
+} 
+
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error){
+        display.value = "Erro";
+    }
+    
+}
